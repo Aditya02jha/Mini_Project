b75b7cabdd921caa83c74d6b1049b11d2726117a
ADITYA JHA <a4adityajha@gmail.com>
2023-12-17 21:49:26 +0530

    added code runner

--- interviewSync_360/src/Components/Room/Collab_editor.jsx
+++ interviewSync_360/src/Components/Room/Collab_editor.jsx
@@ -8,6 +8,7 @@ const CollabEditor = ({ socket, remoteSocketId }) => {
   const [editor, setEditor] = useState(null);
   const editorRef = useRef(null);
   const isRemoteChange = useRef(false);
+  const [output, setOutput] = useState("");
 
   useEffect(() => {
     if (!editorRef.current) {
@@ -51,12 +52,37 @@ const CollabEditor = ({ socket, remoteSocketId }) => {
     }
   };
 
+  function handleRun() {
+    const code = editorRef.current.getValue();
+    try {
+      const result = eval(code);
+      setOutput(result);
+      console.log(result);
+    } catch (err) {
+      console.log("error in handle run \n", err);
+    }
+  }
+
   return (
     <div>
+      <button
+        onClick={handleRun}
+        className="bg-blue-500 text-white py-2 px-4 rounded-full mb-4"
+      >
+        RUN
+      </button>
+
       <div
         id="code-editor"
-        className="shadow-xl shadow-black border-spacing-2 border-black w-full rounded-lg m-3 p-4"
+        className="shadow-xl shadow-black border-spacing-2 border-black w-full rounded-lg m-3 p-4 overflow-auto"
       />
+
+      <div
+        id="code_result"
+        className="bg-black text-white w-full rounded-lg m-3 p-4"
+      >
+        {output}
+      </div>
     </div>
   );
 };
